0d16abaee3092ce45de3d01eee2dd1f0d66a3f4f
Yuri <yurichichedom808@gmail,com>
2024-04-05 13:20:42 -0300

    Classe Musica criada, com atributos que vao caracterizar uma musica

---
+++ src/br/com/alura/model/Musica.java
@@ -0,0 +1,22 @@
+package br.com.alura.model;
+
+public class Musica extends Audio{
+
+    private String artista, album;
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
